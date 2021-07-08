07146b13e14bddc7e23174fd76d01a24b24fc864
Bogdan Shmalko <bshmalko97@gmail.com>
2021-07-08 15:51:54 +0300

    style(project): removed double quotes

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import * as fromComponents from './components';
 import { AppMaterialModule } from './material.module';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [...fromComponents.components],
