93ce39078620668575905dfa747669a535e43a18
Bogdan Shmalko <bshmalko97@gmail.com>
2021-07-08 16:22:28 +0300

    fix(async): delete all async pipes

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import * as fromComponents from './components';
 import { AppMaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
+import { ReactiveComponentModule } from '@ngrx/component';
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule, AppMaterialModule, FormsModule],
+  imports: [CommonModule, AppMaterialModule, FormsModule, ReactiveComponentModule],
   exports: [...fromComponents.components]
 })
 export class SharedModule {}
